9f5179d2261902cc742cc66adf41a1c9e812b32f
Fatim Diarrassouba <timazahara86@gmail.com>
2018-11-28 14:30:39 +0100

    Files addes

---
+++ src/main/java/de/FatimD/MavenPraktikum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MavenPraktikum {
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bitte eine Zeichenkette eingeben");
+		String input= scanner.nextLine();
+		System.out.println(input.toUpperCase());
+		
+	}
+
+}
